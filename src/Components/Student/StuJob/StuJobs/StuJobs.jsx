a4df51cce31ee7f1c8ffd89e86b82c2267076017
vishnu nair <infovishnunair@gmail.com>
2024-08-06 18:09:32 +0530

    06-08-2024- job section

--- src/Components/Student/StuJob/StuJobs/StuJobs.jsx
+++ src/Components/Student/StuJob/StuJobs/StuJobs.jsx
@@ -1,11 +1,98 @@
 
+// import React, { useState, useEffect } from 'react';
+// import { getAllJob } from '../../../../Api/Job';
+// import './stujobs.css';
+// import StuJobCard from './Stujobcard/StuJobCard';
+// import Loading from '../../../Loading/Loading';
+// import { useSelector } from 'react-redux';
+// import './stujobs.css'
+// import { BASE_URL } from '../../../../Constants/BaseUrl';
+
+// function Alljobs() {
+//     const userId = useSelector(state => state.user.userDetails.userId);
+//     const [allJobs, setAllJobs] = useState([]);
+//     const [loading, setLoading] = useState(true);
+//     const [error, setError] = useState(null);
+//     const [selectedJob, setSelectedJob] = useState(null);
+
+//     useEffect(() => {
+//         const fetchJobs = async () => {
+//             try {
+//                 const jobData = await getAllJob(userId);
+//                 setAllJobs(jobData);
+//                 setLoading(false);
+//             } catch (error) {
+//                 setError(error);
+//                 setLoading(false);
+//             }
+//         };
+//         if (userId) {
+//             fetchJobs();
+//         }
+//     }, [userId]);
+
+//     if (loading) {
+//         return <Loading />;
+//     }
+
+//     if (error) {
+//         return <div>Error: {error.message}</div>;
+//     }
+
+//     return (
+//         <div className="jobs-container">
+//             <div className="jobs-list">
+//                 {allJobs.length > 0 ? (
+//                     allJobs.map((job) => (
+//                         <div
+//                             key={job._id}
+//                             className="job-item"
+//                             onClick={() => setSelectedJob(job)}
+//                         >
+//                             <div className='job-Details'>
+//                                 <div>
+//                                     <div className='job-files' >
+//                                         {job.CreatedBy?.Logo ? (
+//                                             <img className="logo" src={`${BASE_URL}/UserFiles/${job.CreatedBy?.Logo}`} />
+//                                         ) : (
+//                                             <p>No logo available</p>
+//                                         )}
+//                                     </div>
+
+//                                 </div>
+//                                 <div>
+//                                     <h5><strong>{job.JobTitle}</strong></h5>
+//                                     <p>Location : <strong>{job.Location}</strong></p>
+//                                     <p>Salary : <strong>{job.Salary} </strong>Lacs P.A</p>
+
+//                                 </div>
+
+//                             </div>
+
+
+//                         </div>
+//                     ))
+//                 ) : (<div className="no-jobs">No jobs available</div>)
+
+//                 }
+//             </div>
+//             <div className="job-details">
+//                 <StuJobCard job={selectedJob} />
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Alljobs;
+
+
 import React, { useState, useEffect } from 'react';
 import { getAllJob } from '../../../../Api/Job';
 import './stujobs.css';
 import StuJobCard from './Stujobcard/StuJobCard';
 import Loading from '../../../Loading/Loading';
 import { useSelector } from 'react-redux';
-import './stujobs.css'
+import { BASE_URL } from '../../../../Constants/BaseUrl';
 
 function Alljobs() {
     const userId = useSelector(state => state.user.userDetails.userId);
@@ -48,16 +135,27 @@ function Alljobs() {
                             className="job-item"
                             onClick={() => setSelectedJob(job)}
                         >
-                            <h4>{job.JobTitle}</h4>
-                            <p>Location:<strong>{job.Location}</strong></p>
-                            <p>Salary:<strong>{job.Salary} </strong>Lacs P.A</p>
+                            <div className='job-details'>
+                                <div className='job-logo'>
+                                    {job.CreatedBy?.Logo ? (
+                                        <img className="logo" src={`${BASE_URL}/UserFiles/${job.CreatedBy?.Logo}`} alt="Company Logo" />
+                                    ) : (
+                                        <p>No logo available</p>
+                                    )}
+                                </div>
+                                <div className='job-info'>
+                                    <h5><strong>{job.JobTitle}</strong></h5>
+                                    <p>Location: <strong>{job.Location}</strong></p>
+                                    <p>Salary: <strong>{job.Salary}</strong> Lacs P.A</p>
+                                </div>
+                            </div>
                         </div>
                     ))
-                ) : (<div className="no-jobs">No jobs available</div>)
-
-                }
+                ) : (
+                    <div className="no-jobs">No jobs available</div>
+                )}
             </div>
-            <div className="job-details">
+            <div className="job-details-container">
                 <StuJobCard job={selectedJob} />
             </div>
         </div>
--- src/Components/Student/StuJob/StuJobs/Stujobcard/StuJobCard.jsx
+++ src/Components/Student/StuJob/StuJobs/Stujobcard/StuJobCard.jsx
@@ -7,70 +7,84 @@ function StuJobCard({ job }) {
     return <div>Select a job to see details</div>;
   }
   return (
-
     <div>
+
       <div className='stujobView'>
+
         <div className="job-card" >
+
           <div className='title'>
+
             <div className='job-title'>
               <div><strong>{job.JobTitle}</strong></div>
             </div>
           </div>
+
           <div className='Job-First-box'>
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Location</strong>
-              </div>
-              <div className="">: {job.Location} </div>
+            <div>
+              
             </div>
 
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Qualifications</strong>
+            <div>
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Location</strong>
+                </div>
+                <div className="">: {job.Location} </div>
               </div>
-              <div className="">: {job.Qualifications} </div>
+
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Qualifications</strong>
+                </div>
+                <div className="">: {job.Qualifications} </div>
+              </div>
+
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Experience</strong>
+                </div>
+                <div className="">: {job.Experience} years</div>
+              </div>
+
+              <div className="jobRow Applicants">
+                <div className="jobLabel">
+                  <strong>No of Applicants</strong>
+                </div>
+                <div className="">:</div>
+              </div>
+
             </div>
 
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Experience</strong>
+            <div>
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Role</strong>
+                </div>
+                <div className="">: {job?.JobTitle} </div>
               </div>
-              <div className="">: {job.Experience} years</div>
-            </div>
 
-            <div className="jobRow Applicants "  >
-              <div className="jobLabel">
-                <strong>No of Applicants</strong>
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Salary</strong>
+                </div>
+                <div className="">: {job?.Salary ? `${job?.Salary} Lacs P.A. ` : "Not Disclosed"}</div>
               </div>
-              {/* <div className="">: {job?.AppliedStudents?.length}</div> */}
-            </div>
 
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Role</strong>
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Openings</strong>
+                </div>
+                <div className="">: {job.Openings} </div>
               </div>
-              <div className="">: {job?.JobTitle} </div>
-            </div>
 
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Salary</strong>
+              <div className="jobRow">
+                <div className="jobLabel">
+                  <strong>Employment Type:</strong>
+                </div>
+                <div className="">: {job?.EmploymentType} </div>
               </div>
-              <div className="">: {job?.Salary ? `${job?.Salary} Lacs P.A. ` : "Not Disclosed"}</div>
-            </div>
 
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Openings</strong>
-              </div>
-              <div className="">: {job.Openings} </div>
-            </div>
-
-            <div className="jobRow">
-              <div className="jobLabel">
-                <strong>Employment Type:</strong>
-              </div>
-              <div className="">: {job?.EmploymentType} </div>
             </div>
 
             <div className="jobRow">
